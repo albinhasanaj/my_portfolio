c99e30537eed37eb9664fe6ab7b26799bfe027c4
PoorGenius <albinhasanaj06@hotmail.com>
2024-05-02 22:02:39 +0200

    Heading changed

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -18,10 +18,10 @@ const ProjectsList = ({ title, description, logo, tags, colors, github }: Projec
 
 
     return (
-        <div className="card__background max-w-[1000px] w-2/3 h-auto py-12 flex flex-col rounded-[25px] overflow-hidden">
+        <div className="card__background max-w-[950px] w-2/3 h-auto py-12 flex flex-col rounded-[25px] overflow-hidden">
             <div className="flex flex-col gap-12">
 
-                <div className="flex w-full h-full items-center gap-2 justify-between px-10 lg:px-40">
+                <div className="flex w-full h-full items-center gap-2 justify-between px-10 lg:px-20">
                     <Image
                         src="/images/expand.png"
                         width={32}
@@ -30,13 +30,13 @@ const ProjectsList = ({ title, description, logo, tags, colors, github }: Projec
                         className={`cursor-pointer ${isExpanded ? 'rotate-180' : ''} transition-transform duration-300 w-[24px] h-auto lg:w-[32px]`}
                         onClick={() => setIsExpanded(prev => !prev)}
                     />
-                    <h3 className="text-center lg:text-[24px] font-bold">{title}</h3>
+                    <h5 className="heading5 font-bold">{title}</h5>
                     <Image
                         src={logo}
                         height={64}
                         width={64}
                         alt={title}
-                        className="hidden sm:flex w-[64px] h-auto"
+                        className="hidden sm:flex w-[48px] md:w-[64px] h-auto"
                     />
                 </div>
                 <AnimatePresence>
@@ -47,13 +47,13 @@ const ProjectsList = ({ title, description, logo, tags, colors, github }: Projec
                             exit={{ opacity: 0, height: 0 }}
                             transition={{ duration: 0.3 }}
                             className="flex flex-col items-center gap-12">
-                            <p className="text-center w-2/3 text-[16px] lg:text-[20px]">{description}</p>
+                            <p className="text-center paragraph-contained">{description}</p>
                             <div className="flex flex-col gap-4 lg:flex-row w-full items-center justify-between px-20 lg:px-40">
                                 <div className="flex gap-2 flex-wrap justify-center">
                                     {tags.map((tag, i) => (
                                         <span key={tag}
                                             style={{ color: colors[i] }}
-                                            className="text-[16px] bg-[rgba(255,255,255,0.1)] rounded-[5px]"
+                                            className="span bg-[rgba(255,255,255,0.1)] rounded-[5px]"
                                         >{tag}</span>
 
 
