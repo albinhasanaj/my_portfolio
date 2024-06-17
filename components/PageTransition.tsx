9111cd14a0d704b2ca60085ea94eec55cdee2f78
PoorGenius <albinhasanaj06@hotmail.com>
2024-06-17 19:09:59 +0200

    feat: Add loading spinner to page transition

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { motion, AnimatePresence } from "framer-motion";
+import { ClimbingBoxLoader } from "react-spinners";
 import useStore from "@/store/useStore";
-import StarsBackGround from "./StarsBackGround";
-
 const PageTransition = () => {
     const isChangingPage = useStore((state) => state.isChangingPage);
     return (
@@ -10,12 +9,15 @@ const PageTransition = () => {
             {isChangingPage && (
                 <>
                     <motion.div
-                        key="page-transition"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
-                        className="fixed inset-0 bg-[#09090B] z-[11]"
-                    />
+                        className="fixed inset-0 bg-[#09090B] z-[11] flex items-center justify-center"
+                    >
+
+                        <ClimbingBoxLoader color="#018EBB" size={25} speedMultiplier={1.5} />
+
+                    </motion.div>
                 </>
 
             )}
